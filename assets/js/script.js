a3fd52dca6528cc8d603e55968603f94fcb6da87
BritishPandaChick <sbartley1080@gmail.com>
2019-10-30 18:55:06 -0400

    Update all code files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,34 @@
-// jquery
+//jQuery time
 
 //add '.ready' to form when user focuses on it
 $("#email").focus(function(){
-  $("#cuboid form").addClass("ready");
-});
+	$("#cuboid form").addClass("ready");
+})
 //remove '.ready' when user blus away but only if there is no content
 $("#email").blur(function(){
-  if($(this).val() == "")
-  $("#cuboid form").removeClass("ready");
-});
+	if($(this).val() == "")
+		$("#cuboid form").removeClass("ready");
+})
 
-//If user is typing something that makes the arrow green/.active
+//If the user is typing something make the arrow green/.active
 $("#email").keyup(function(){
-  //this adds .active class only if the input has some text
-  $(".submit-icon").toggleClass("active", $(this).val().length > 0);
-});
+	//this adds .active class only if the input has some text
+	$(".submit-icon").toggleClass("active", $(this).val().length > 0);
+})
 
 //on form submit remove .ready and add .loading to the form
 $("#cuboid form").submit(function(){
-  $(this).removeClass("ready").addClass("loading");
-  //finish loading in 3s
-  setTimeout(complete, 3000);
-  //prevent default form submission
-  return false;
-});
-
-function complete() {
-  $("#cuboid form").removeClass("loading").addClass("complete");
+	$(this).removeClass("ready").addClass("loading");
+	//finish loading in 3s
+	setTimeout(complete, 3000);
+	//prevent default form submisson
+	return false;
+})
+function complete()
+{
+	$("#cuboid form").removeClass("loading").addClass("complete");
 }
 //reset/refresh functionality
 $(".reset-icon").click(function(){
-  $("#cuboid form").removeClass("complete");
-});
+	$("#cuboid form").removeClass("complete");
+})
